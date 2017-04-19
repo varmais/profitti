2f503354d6253c95c8357136dba06ed070f4d7ac
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2017-04-19 20:01:13 +0300

    Update navigation component tests

--- src/components/navigation/NavigationButton.spec.js
+++ src/components/navigation/NavigationButton.spec.js
@@ -1,30 +1,69 @@
-import '../../testutils';
 import NavigationButton from './NavigationButton';
+import config from '../../config';
 
 describe('components:navigation:NavigationButton', () => {
   let onPressStub;
+  let component;
   const text = 'button text';
   const icon = 'glass';
 
   beforeEach(() => {
-    onPressStub = jest.fn();
+    onPressStub = sinon.stub();
+    component = shallow(<NavigationButton text={text} onPress={onPressStub} />);
   });
 
-  describe('when icon has not been given', () => {
-    test('renders NavigationButton', () => {
-      const snapshot = renderer.create(<NavigationButton text={text} onPress={onPressStub} />).toJSON();
-      expect(snapshot).toMatchSnapshot();
+  it('renders NavigationButton', () => {
+    expect(component.prop('underlayColor')).to.eql(config.colors.gray);
+  });
+
+  it('renders text', () => {
+    expect(component.find('Text').children().text()).to.eql(text);
+  });
+
+  it('renders arrow', () => {
+    expect(component.find('Icon').props()).to.eql({
+      allowFontScaling: false,
+      name: 'ios-arrow-forward',
+      size: 24,
+      style: {
+        bottom: 15,
+        position: 'absolute',
+        right: 0
+      }
+    });
+  });
+
+  it('does not render icon', () => {
+    expect(component.find('Icon')).to.have.length(1);
+  });
+
+  describe('when button is pressed', () => {
+    beforeEach(() => {
+      component.simulate('press');
     });
 
-    describe('when button is pressed', () => {
-      test('it calls onPress callback');
+    it('it calls onPress callback', () => {
+      expect(onPressStub).to.have.been.calledOnce;
     });
   });
 
   describe('when icon is given', () => {
-    test('renders NavigationButton', () => {
-      const snapshot = renderer.create(<NavigationButton icon={icon} text={text} onPress={onPressStub} />).toJSON();
-      expect(snapshot).toMatchSnapshot();
+    beforeEach(() => {
+      component.setProps({icon});
+    });
+
+    it('renders icon', () => {
+      expect(component.find('Icon')).to.have.length(2);
+      expect(component.find('Icon').first().props()).to.eql({
+        allowFontScaling: false,
+        name: icon,
+        size: 18,
+        style: {
+          bottom: 20,
+          left: 0,
+          position: 'absolute'
+        }
+      });
     });
   });
 });
--- src/components/navigation/NavigationButtonListView.spec.js
+++ src/components/navigation/NavigationButtonListView.spec.js
@@ -1,19 +1,35 @@
-import {createNavigationProp} from '../../testutils';
-import { View } from 'react-native';
+import { createNavigationProp } from '../../testutils';
+import { View, ScrollView } from 'react-native';
 import NavigationButtonListView from './NavigationButtonListView';
+import Separator from '../common/Separator';
 
 describe('components:navigation:NavigationButtonListView', () => {
+  let component;
   const renderRow = () => <View />;
   const items = [{item: 1}, {item: 4}];
   const title = 'title';
 
-  test('renders NavigationButtonListView', () => {
-    const snapshot = renderer.create(<NavigationButtonListView
+  beforeEach(() => {
+    component = shallow(<NavigationButtonListView
       items={items}
       renderRow={renderRow}
       title={title}
       navigation={createNavigationProp()}
-    />).toJSON();
-    expect(snapshot).toMatchSnapshot();
+    />);
+  });
+
+  it('renders title', () => {
+    expect(component.find('Text').children().text()).to.eql(title);
+  });
+
+  it('renders list view', () => {
+    const props = component.find('ListView').props();
+    expect(props.title).to.eql(title);
+    expect(props.items).to.eql(items);
+    expect(props.renderRow).to.eql(renderRow);
+    expect(props.renderSeparator()).to.eql(<Separator/>);
+    expect(props.dataSource).to.eql({
+      _dataBlob: items
+    });
   });
 });
